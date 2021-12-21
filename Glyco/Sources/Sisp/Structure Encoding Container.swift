fd84f588ec4720dc57515c4b094db81ed3626dea
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2021-12-21 11:52:21 +0100

    Adds support for encoding Sisp structures of unnamed type.

--- Glyco/Sources/Sisp/Structure Encoding Container.swift
+++ Glyco/Sources/Sisp/Structure Encoding Container.swift
@@ -2,6 +2,13 @@
 
 struct StructureEncodingContainer<Key : CodingKey> : KeyedEncodingContainerProtocol {
 	
+	/// Creates a container for encoding to a structure.
+	init(encoder: SispEncoder, indexPath: [Sisp.Index], codingPath: [CodingKey]) {
+		self.encoder = encoder
+		self.indexPath = indexPath
+		self.codingPath = codingPath
+	}
+	
 	/// The encoder.
 	let encoder: SispEncoder
 	
@@ -11,89 +18,92 @@ struct StructureEncodingContainer<Key : CodingKey> : KeyedEncodingContainerProto
 	// See protocol.
 	let codingPath: [CodingKey]
 	
+	/// A Boolean value indicating whether a structure has been encoded.
+	private var encodedStructure = false
+	
 	// See protocol.
 	mutating func encodeNil(forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encodeNil()
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Bool, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: String, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Double, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Float, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Int, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Int8, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Int16, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Int32, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: Int64, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: UInt, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: UInt8, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: UInt16, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: UInt32, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode(_ value: UInt64, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func encode<T : Encodable>(_ value: T, forKey key: Key) throws {
-		TODO.unimplemented	// raw value encoding
+		try singleValueContainer(forKey: key).encode(value)
 	}
 	
 	// See protocol.
 	mutating func nestedContainer<NestedKey : CodingKey>(keyedBy keyType: NestedKey.Type, forKey key: Key) -> KeyedEncodingContainer<NestedKey> {
-		encoder.sisp[indexPath] = .structure(type: key.stringValue, children: [:])
+		encodeEmptyStructureIfNeeded(type: key.stringValue)
 		return .init(StructureBodyEncodingContainer(
 			encoder:	encoder,
 			indexPath:	indexPath,
@@ -103,7 +113,11 @@ struct StructureEncodingContainer<Key : CodingKey> : KeyedEncodingContainerProto
 	
 	// See protocol.
 	mutating func nestedUnkeyedContainer(forKey key: Key) -> UnkeyedEncodingContainer {
-		TODO.unimplemented	// unlabelled child list?
+		ListEncodingContainer(
+			encoder:	encoder,
+			indexPath:	indexPath + [.label(.init(rawValue: key.stringValue))],
+			codingPath:	codingPath + [key]
+		)
 	}
 	
 	// See protocol.
@@ -116,5 +130,21 @@ struct StructureEncodingContainer<Key : CodingKey> : KeyedEncodingContainerProto
 		TODO.unimplemented
 	}
 	
+	/// Returns a single value container for encoding a value at `key`.
+	private mutating func singleValueContainer(forKey key: Key) -> SingleValueEncodingContainer {
+		encodeEmptyStructureIfNeeded(type: key.stringValue)
+		return .init(
+			encoder:	encoder,
+			indexPath:	indexPath + [.label(.init(rawValue: key.stringValue))],
+			codingPath:	codingPath + [key]
+		)
+	}
+	
+	/// Encodes an empty structure, ready to be written to.
+	private mutating func encodeEmptyStructureIfNeeded(type: String?) {
+		guard !encodedStructure else { return }
+		encoder.sisp[indexPath] = .structure(type: type, children: [:])
+		encodedStructure = true
+	}
 	
 }
