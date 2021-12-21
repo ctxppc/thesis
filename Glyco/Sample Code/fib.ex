3d88ea0bbd7a2858a6e3460ab9a37bb88d395fae
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2021-12-21 14:18:34 +0100

    Fixes sample code bug.

--- Glyco/Sample Code/fib.ex
+++ Glyco/Sample Code/fib.ex
@@ -1,12 +1,12 @@
 program(
-	invoke(fib, constant(0) constant(0) constant(30)),
+	invoke(fib, constant(0) constant(1) constant(30)),
 	procedures: 
 		procedure(fib, parameters: parameter(prev, type: word) parameter(curr, type: word) parameter(iterations, type: word),
 		if(relation(location(iterations), le, immediate(0)),
 			then:	return(location(curr)),
 			else:	sequence(
 				assign(next, to: binary(location(prev), add, location(curr)))
-				assign(limit, to: binary(location(limit), sub, constant(1)))
+				assign(limit, to: binary(location(iterations), sub, constant(1)))
 				invoke(fib, location(curr) location(next) location(limit))
 			)
 		)
