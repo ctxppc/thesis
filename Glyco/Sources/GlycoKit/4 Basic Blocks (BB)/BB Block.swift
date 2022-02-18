606e163d408833470c76b07c85eaf1efc76ce148
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2022-02-18 15:09:21 +0100

    Fixes an issue in BB where a block with a call continuation doesn’t lower the block’s effects.
    Signed-off-by: Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>

--- Glyco/Sources/GlycoKit/4 Basic Blocks (BB)/BB Block.swift
+++ Glyco/Sources/GlycoKit/4 Basic Blocks (BB)/BB Block.swift
@@ -1,5 +1,7 @@
 // Glyco © 2021–2022 Constantino Tsarouhas
 
+import DepthKit
+
 extension BB {
 	
 	/// A sequence of effects with a single entry and exit point.
@@ -23,35 +25,9 @@ extension BB {
 		
 		// See protocol.
 		public func lowered(in context: inout ()) throws -> [Lower.Effect] {
-			switch continuation {
-				
-				case .continue(to: let successor):
-				if let (first, tail) = try effects.lowered().splittingFirst() {
-					return [.labelled(name, first)] + tail + [.jump(to: successor)]
-				} else {
-					return [.labelled(name, .jump(to: successor))]
-				}
-				
-				case .branch(let lhs, let relation, let rhs, then: let affirmative, else: let negative):
-				if let (first, tail) = try effects.lowered().splittingFirst() {
-					return [.labelled(name, first)]
-						+ tail
-						+ [.branch(to: affirmative, lhs, relation, rhs), .jump(to: negative)]
-				} else {
-					return [.labelled(name, .branch(to: affirmative, lhs, relation, rhs)), .jump(to: negative)]
-				}
-				
-				case .call(let name, returnPoint: let returnPoint):
-				return [.call(name), .jump(to: returnPoint)]
-				
-				case .return:
-				if let (first, tail) = try effects.lowered().splittingFirst() {
-					return [.labelled(name, first)] + tail + [.return]
-				} else {
-					return [.labelled(name, .return)]
-				}
-				
-			}
+			let loweredEffects = try effects.lowered() + continuation.lowered()
+			let (first, tail) = loweredEffects.splittingFirst() !! "Expected lowered effect for continuation"
+			return [.labelled(name, first)] + tail
 		}
 		
 	}
