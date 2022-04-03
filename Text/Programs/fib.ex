82e8e23fd1456008271ea9c70c3945d716abf32d
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2022-04-03 20:08:24 +0200

    Extends Glyco chapter.
    Signed-off-by: Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>

---
+++ Text/Programs/fib.ex
@@ -0,0 +1,14 @@
+(
+	evaluate(fib, constant(0) constant(1) constant(30)),
+	functions:
+		(fib, takes: (prev, s32()) (curr, s32()) (iter, s32()), returns: s32(), in:
+			if(relation(named(iter), le, constant(0)),
+				then:	value(named(curr)),
+				else:	evaluate(fib, 
+					named(curr) 
+					binary(named(prev), add, named(curr)) 
+					binary(named(iter), sub, constant(1))
+				)
+			)
+		)
+)
