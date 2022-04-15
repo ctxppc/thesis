6585776a0d8d7156d3af7023901471eb251d802b
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2022-04-15 17:13:12 +0200

    Updates tests.
    Signed-off-by: Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>

--- Glyco/Tests/Test Programs/conventional.sail/sum.s
+++ Glyco/Tests/Test Programs/conventional.sail/sum.s
@@ -45,8 +45,8 @@ rv.runtime:		cllc ct0, mm.heap
 				auipcc ct0, 0
 				addi t1, zero, 129
 				candperm ct0, ct0, t1
-				addi t1, t1, 1
-				slli t1, t1, 19
+				addi t1, zero, 1
+				slli t1, t1, 17
 				csetaddr ct0, ct0, t1
 				cllc ct1, mm.cseal_seal_cap
 				csc ct0, 0(ct1)
