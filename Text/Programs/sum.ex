6ca84b5387dd40af8df621318d125c9b7d7d5db0
Constantino Tsarouhas <c@tsarouhas.eu>
2022-04-27 00:10:38 +0200

    Describes lambdas and named/nominal types in objects chapter.

---
+++ Text/Programs/sum.ex
@@ -0,0 +1,9 @@
+(
+	evaluate(function(l.anon), constant(1080) constant(-80)),
+	functions: (
+		l.anon,
+		takes: (first, s32(), sealed: false) (second, s32(), sealed: false),
+		returns: s32(),
+		in: value(binary(named(first), add, named(second)))
+	)
+)
