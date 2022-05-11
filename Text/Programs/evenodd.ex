d6921006aaad5f1102e2c1d65a45f4d4899534f2
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2022-05-11 21:00:28 +0200

    Improves objects chapter.
    Signed-off-by: Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>

---
+++ Text/Programs/evenodd.ex
@@ -0,0 +1,25 @@
+(
+	let((even, function(l.even)) (odd, function(l.odd)), in: evaluate(even, 420)),
+	functions: (
+		l.even,
+		takes: (n, s32, sealed: false),
+		returns: s32,
+		in: let(
+			(even, function(l.even)) (odd, function(l.odd)),
+			in: if(relation(n, le, 0), then: value(1), else: evaluate(odd, binary(n, sub, 1)))
+		)
+	)
+	(
+		l.odd,
+		takes: (n, s32, sealed: false),
+		returns: s32,
+		in: let(
+			(even, function(l.even)) (odd, function(l.odd)),
+			in: if(
+				relation(n, le, 0),
+				then: value(0),
+				else: evaluate(even, binary(n, sub, 1))
+			)
+		)
+	)
+)
