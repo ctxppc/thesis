10a9e7cf1eb06f5d9299c8691f2a8f66d8533e5c
Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>
2022-05-18 14:11:15 +0200

    Updates example programs in text.
    Signed-off-by: Constantino Tsarouhas <constantino.tsarouhas@student.kuleuven.be>

--- Text/Programs/values.cc
+++ Text/Programs/values.cc
@@ -6,10 +6,10 @@
 		createRecord(
 			((name, cap(vector(of: u8, sealed: false))) (age, s32)),
 			capability: d,
-			scoped: true
+			scoped: false
 		)
 		getField(name, of: d, to: e)
-		createVector(s32, count: 100, capability: f, scoped: true)
+		createVector(s32, count: 100, capability: f, scoped: false)
 		getElement(of: f, index: 50, to: g)
 		return(g)
 	),
